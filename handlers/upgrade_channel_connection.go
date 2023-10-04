476e76405b41e48603217d59058fd041b1376695
Gustavo Bertoi <gustavobertoi@outlook.com>
2023-10-04 19:02:12 -0300

    refactor(handlers): refactoring upgrading connection handler ws and sse

--- handlers/upgrade_channel_connection.go
+++ handlers/upgrade_channel_connection.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"sync"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"github.com/open-source-cloud/realtime/channels"
 	"github.com/open-source-cloud/realtime/config"
 )
@@ -68,112 +65,12 @@ func UpgradeChannelConnectionHandler(c *gin.Context) {
 
 	switch channel.Type {
 	case channels.WebSocket:
-		ws(c, channel, client, logger)
+		WebSocketHandler(c, channel, client, logger)
 	case channels.ServerSentEvents:
-		sse(c, channel, client, logger)
+		ServerSentEventsHandler(c, channel, client, logger)
 	}
 
 	channel.DeleteClient(client)
 
 	logger.Warnf("the channel %s connection with the client %s has ended", channel.ID, client.ID)
-}
-
-func ws(c *gin.Context, channel *channels.Channel, client *channels.Client, logger *config.Logger) {
-	upgrader := websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-	}
-
-	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
-		logger.Errorf("error upgrading connection to websocket, err: %v", err)
-		panic(err)
-	}
-
-	msgChan := client.MessageChan()
-	go func() {
-		for {
-			msg := <-msgChan
-			if msg.ClientID != client.ID {
-				logger.Infof("serializing and writing msg %s to buffer", msg.ID)
-				msgStr, err := msg.MessageToJSON()
-				if err != nil {
-					logger.Errorf("error serializing msg %s to json, err: %v", msg.ID, err)
-					break
-				}
-				if err := conn.WriteMessage(websocket.TextMessage, []byte(msgStr)); err != nil {
-					logger.Errorf("error writing msg %s on buffer, err: %v", msg.ID, err)
-					break
-				}
-				logger.Infof("msg %s was written to buffer for client %s", msg.ID, client.ID)
-			} else {
-				logger.Warnf("not writing self msg %s to this client %s", msg.ID, client.ID)
-			}
-		}
-	}()
-
-	// Closes WS client connection
-	defer conn.Close()
-
-	// Read WS messages
-	for {
-		messageType, payload, err := conn.ReadMessage()
-		if err != nil {
-			logger.Errorf("error reading message from buffer, err: %v", err)
-			break
-		}
-		msg := channels.NewMessage(channel.ID, client.ID, string(payload))
-		logger.Infof("sending %s msg to all clients", msg.ID)
-		if messageType == websocket.TextMessage {
-			if err := channel.BroadcastMessage(msg); err != nil {
-				logger.Errorf("error broadcasting msg %s to clients, err: %v", msg.ID, err)
-				break
-			}
-			logger.Infof("msg %s has been sent to all client", msg.ID)
-		}
-	}
-}
-
-func sse(c *gin.Context, channel *channels.Channel, client *channels.Client, logger *config.Logger) {
-	c.Writer.Header().Set("Content-Type", "text/event-stream")
-	c.Writer.Header().Set("Cache-Control", "no-cache")
-	c.Writer.Header().Set("Connection", "keep-alive")
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-
-	msgChan := client.MessageChan()
-	clientGone := c.Writer.CloseNotify()
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-clientGone:
-				return
-			case msg := <-msgChan:
-				logger.Infof("serializing and writing msg %s to client %s", msg.ID, client.ID)
-				if msg.ClientID != client.ID {
-					msgStr, err := msg.MessageToJSON()
-					if err != nil {
-						logger.Errorf("error serializing msg %s to json, err: %v", msg.ID, err)
-						break
-					}
-					message := fmt.Sprintf("data: %s\n\n", msgStr)
-					_, err = c.Writer.WriteString(message)
-					if err != nil {
-						logger.Errorf("error writing msg %s on buffer, err: %v", msg.ID, err)
-						break
-					}
-					c.Writer.Flush()
-					logger.Infof("msg %s was written to buffer for client %s", msg.ID, client.ID)
-					time.Sleep(1 * time.Second)
-				} else {
-					logger.Warnf("not writing self msg %s to client %s", msg.ID, client.ID)
-				}
-			}
-		}
-	}()
-
-	wg.Wait()
 }
